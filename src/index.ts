9f72850590bb897ad755a7ee94df1c254d3d5bd2
Roj <ez@roj.im>
2024-01-01 22:18:24 +0300

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,79 @@
-import { Storage, StorageKeyPart } from "@mtkruto/node";
+import { GetManyFilter, Storage, StorageKeyPart } from "@mtkruto/node";
 import { LocalStorage } from "node-localstorage";
-import { fromString, toString } from "./utilities";
+import { fromString, isInRange, toString } from "./utilities";
 
 export class StorageLocalStorage extends Storage implements Storage {
-  constructor(
-    private readonly prefix: string,
-    private readonly localStorage = new LocalStorage(".mtkruto"),
-  ) {
+  readonly #prefix: string;
+  readonly #localStorage: LocalStorage;
+
+  constructor(prefix: string, localStorage?: LocalStorage) {
     if (prefix.length <= 0) {
       throw new Error("Empty prefix");
     } else if (!/^[0-9a-zA-Z]+$/.test(prefix)) {
       throw new Error("Unallowed prefix");
     }
     super();
+    this.#prefix = prefix;
+    this.#localStorage = localStorage ?? new LocalStorage(".mtkruto");
   }
 
-  init() {
+  get prefix() {
+    return this.#prefix;
   }
 
-  get(key_: readonly StorageKeyPart[]) {
+  branch(id: string) {
+    return new StorageLocalStorage(this.prefix + "S__" + id);
+  }
+
+  initialize() {
+  }
+
+  get supportsFiles() {
+    return false;
+  }
+
+  get<T>(key_: readonly StorageKeyPart[]) {
     const key = this.prefix + toString(key_);
-    const value = this.localStorage.getItem(key);
+    const value = this.#localStorage.getItem(key);
     if (value != null) {
-      return fromString(value);
+      return fromString<T>(value);
     } else {
       return null;
     }
   }
 
-  *getMany<T>(prefix: readonly StorageKeyPart[]) {
-    for (let [key, value] of Object.entries(this.localStorage)) {
+  *getMany<T>(
+    filter: GetManyFilter,
+    params?: { limit?: number; reverse?: boolean },
+  ) {
+    let entries = Object.entries(localStorage).sort(([a], [b]) =>
+      a.localeCompare(b)
+    );
+    if (params?.reverse) {
+      entries.reverse();
+    }
+    if (params?.limit !== undefined) {
+      entries = entries.slice(0, params.limit <= 0 ? 1 : params.limit);
+    }
+    entries: for (let [key, value] of entries) {
       if (key.startsWith(this.prefix)) {
         key = key.slice(this.prefix.length);
       }
       const parts = fromString(key);
       if (Array.isArray(parts)) {
-        for (const [i, p] of prefix.entries()) {
-          if (toString(p) != toString(parts[i])) {
+        if ("prefix" in filter) {
+          for (const [i, p] of filter.prefix.entries()) {
+            if (toString(p) != toString(parts[i])) {
+              continue entries;
+            }
+          }
+        } else {
+          if (!isInRange(parts, filter.start, filter.end)) {
             continue;
           }
-          yield [parts, fromString(value)] as [readonly StorageKeyPart[], T];
         }
+
+        yield [parts, fromString(value)] as [readonly StorageKeyPart[], T];
       }
     }
   }
@@ -48,9 +81,13 @@ export class StorageLocalStorage extends Storage implements Storage {
   set(key_: readonly StorageKeyPart[], value: unknown) {
     const key = this.prefix + toString(key_);
     if (value != null) {
-      this.localStorage.setItem(key, toString(value));
+      this.#localStorage.setItem(key, toString(value));
     } else {
-      this.localStorage.removeItem(key);
+      this.#localStorage.removeItem(key);
     }
+  }
+
+  incr(key: readonly StorageKeyPart[], by: number) {
+    this.set(key, (this.get<number>(key) || 0) + by);
   }
 }
